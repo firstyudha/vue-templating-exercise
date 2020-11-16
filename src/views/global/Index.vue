d7b98fbd6129ec380c1e0454175675c332faddb7
Yudha Pratama <yudha.p27@gmail.com>
2020-11-16 11:50:30 +0700

    data per country and per province

--- src/views/global/Index.vue
+++ src/views/global/Index.vue
@@ -8,37 +8,102 @@
             <div class="container">
                 <div class="row">
 
-                    <div class="col-lg-5 col-md-6">
+                    <div class="col-lg-8 offset-lg-2 col-md-8 col-md-2">
                         <div class="about-img" >
-                            <img src="assets/img/global.jpg" alt="">
+                            <img :src="covid19.image" alt="">
                         </div>
                     </div>
 
-                    <div class="col-lg-7 col-md-6">
-                        <div class="about-content" >
-                            <h2>Global</h2>
-                            <h3>Data Source : https://covid19.mathdro.id/api</h3>
-                            <div class="row mt-5">
-                                <div class="col" v-if="covid19">
-                                    <i class="ion-android-checkmark-circle"></i> Confirmed 
-                                    <h3 class="ml-4 mt-2">{{new Intl.NumberFormat().format(covid19.confirmed.value)}}</h3>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-4 col-md-4">
+                    
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        
+                        <center><h2 class="mt-5">{{selectedCountry}}</h2></center>
+                        <select v-model="selectedCountry" @change="getDataPerCountry" class="form-control mt-1">
+                            <option v-bind:key="Global">Global</option>
+                            <option v-for="country in countries" v-bind:key="country.name">
+                                    {{ country.name }}
+                            </option>
+                        </select>
+                        <small class="text-danger">*Choose country</small>                        
+                        
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                    
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-4">
+                        <div class="card shadow bg-danger text-white w-100 mt-5">
+                            <div class="card-body">
+                                <div class="row" v-if="covidPerCountry">
+                                    <div class="col-4">
+                                        <i class="fas fa-5x fa-viruses"></i>
+                                    </div>
+                                    <div class="col-8">
+                                        <h3>Confirmed</h3>
+                                        <h3><b>{{new Intl.NumberFormat().format(covidPerCountry.confirmed.value)}}</b></h3>
+                                    </div>
                                 </div>
-                                <div class="col" v-else>
-                                    <ContentLoader/>
+                                <div class="row" v-else>
+                                    <div class="col-md-6">
+                                        <CodeLoader/>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <CodeLoader/>
+                                    </div>
                                 </div>
-                                <div class="col" v-if="covid19">
-                                    <i class="ion-android-checkmark-circle"></i> Recovered 
-                                    <h3 class="ml-4 mt-2">{{new Intl.NumberFormat().format(covid19.recovered.value)}}</h3>
+                            </div>
+                        </div>
+                        <p class="ml-1 mt-4">Data Source : https://covid19.mathdro.id/api</p>
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        <div class="card shadow bg-success text-white w-100 mt-5">
+                            <div class="card-body">
+                                <div class="row" v-if="covidPerCountry">
+                                    <div class="col-4">
+                                        <i class="fas fa-5x fa-heartbeat"></i>
+                                    </div>
+                                    <div class="col-8">
+                                        <h3>Recovered</h3>
+                                        <h3><b>{{new Intl.NumberFormat().format(covidPerCountry.recovered.value)}}</b></h3>
+                                    </div>
                                 </div>
-                                <div class="col" v-else>
-                                    <ContentLoader/>
+                                <div class="row" v-else>
+                                    <div class="col-md-6">
+                                        <CodeLoader/>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <CodeLoader/>
+                                    </div>
                                 </div>
-                                <div class="col" v-if="covid19">
-                                    <i class="ion-android-checkmark-circle"></i> Deaths 
-                                    <h3 class="ml-4 mt-2">{{new Intl.NumberFormat().format(covid19.deaths.value)}}</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        <div class="card shadow bg-warning text-white w-100 mt-5">
+                            <div class="card-body">
+                                <div class="row" v-if="covidPerCountry">
+                                    <div class="col-4">
+                                        <i class="fas fa-5x fa-ambulance"></i>
+                                    </div>
+                                    <div class="col-8">
+                                        <h3>Deaths</h3>
+                                        <h3><b>{{new Intl.NumberFormat().format(covidPerCountry.deaths.value)}}</b></h3>
+                                    </div>
                                 </div>
-                                <div class="col" v-else>
-                                    <ContentLoader/>
+                                <div class="row" v-else>
+                                    <div class="col-md-6">
+                                        <CodeLoader/>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <CodeLoader/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -55,7 +120,7 @@
 <script>
     //import content loader
     import {
-        ContentLoader ,
+        CodeLoader ,
     } from 'vue-content-loader'
 
     //import axios
@@ -69,7 +134,7 @@
         name: 'global',
         components: {
             //loader component
-            ContentLoader ,
+            CodeLoader ,
             //component app
             Header,
             Footer
@@ -78,14 +143,46 @@
             return {
                 //define properties
                 covid19: null,
+                countries: null,
+                selectedCountry:'Global',
+                covidPerCountry:null
+            }
+        },
+        methods:{
+            getDataPerCountry(){
+                if(this.selectedCountry === 'Global'){
+                    axios.get('https://covid19.mathdro.id/api').then(response => {
+                        this.covidPerCountry = response.data;
+                    })
+                }else{
+                    axios.get(`https://covid19.mathdro.id/api/countries/`+this.selectedCountry).then(response => {     
+                        this.covidPerCountry = response.data;
+                        console.log(this.covidPerCountry);
+                    })
+                }
             }
         },
         created() {
-            //get post homepage
+            //get global
             axios.get('https://covid19.mathdro.id/api').then(response => {
                 this.covid19 = response.data;
-                console.log(this.covid19);
             })
+
+            //get countries
+            axios.get('https://covid19.mathdro.id/api/countries').then(response => {
+                this.countries = response.data.countries;
+                console.log(this.countries);
+            })
+
+
+        },
+        watch: {
+            $route: {
+                immediate: true,
+                handler() {
+                    this.getDataPerCountry()
+                }
+            }
         }
     }
 </script>
--- src/views/indonesia/Index.vue
+++ src/views/indonesia/Index.vue
@@ -6,42 +6,107 @@
         <section id="about" class="about">
 
             <div class="container">
-                <div class="row">
+                <!-- <div class="row">
 
-                    <div class="col-lg-5 col-md-6">
+                    <div class="col-lg-8 offset-lg-2 col-md-8 col-md-2">
                         <div class="about-img" >
-                            <img src="assets/img/indo.jpg" alt="">
+                            <img :src="covid19.image" alt="">
                         </div>
                     </div>
 
-                    <div class="col-lg-7 col-md-6">
-                        <div class="about-content" >
-                            <h2>Indonesia</h2>
-                            <h3>Data Source : https://indonesia-covid-19.mathdro.id/api</h3>
-                            <div class="row mt-5">
-                                <div class="col" v-if="covid19">
-                                    <i class="ion-android-checkmark-circle"></i> Confirmed 
-                                    <h3 class="ml-4 mt-2">{{new Intl.NumberFormat().format(covid19.jumlahKasus)}}</h3>
+
+                </div> -->
+
+                <div class="row">
+                    <div class="col-lg-4 col-md-4">
+                    
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        
+                        <center><h2>{{selectedProv}}</h2></center>
+                        <select v-model="selectedProv" @change="getDataPerProv" class="form-control mt-1">
+                            <option v-for="prov in provs" v-bind:key="prov.provinsi">
+                                    {{ prov.provinsi }}
+                            </option>
+                        </select>
+                        <small class="text-danger">*Choose Province</small>                        
+                        
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                    
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-4">
+                        <div class="card shadow bg-danger text-white w-100 mt-5">
+                            <div class="card-body">
+                                <div class="row" v-if="covidPerProv">
+                                    <div class="col-4">
+                                        <i class="fas fa-5x fa-viruses"></i>
+                                    </div>
+                                    <div class="col-8">
+                                        <h3>Confirmed</h3>
+                                        <h3><b>{{new Intl.NumberFormat().format(covidPerProv[0].kasusPosi)}}</b></h3>
+                                    </div>
                                 </div>
-                                <div class="col" v-else>
-                                    <ContentLoader/>
-                                </div>
-                                <div class="col" v-if="covid19">
-                                    <i class="ion-android-checkmark-circle"></i> Recovered 
-                                    <h3 class="ml-4 mt-2">{{new Intl.NumberFormat().format(covid19.sembuh)}}</h3>
-                                </div>
-                                <div class="col" v-else>
-                                    <ContentLoader/>
-                                </div>
-                                <div class="col" v-if="covid19">
-                                    <i class="ion-android-checkmark-circle"></i> Deaths 
-                                    <h3 class="ml-4 mt-2">{{new Intl.NumberFormat().format(covid19.meninggal)}}</h3>
-                                </div>
-                                <div class="col" v-else>
-                                    <ContentLoader/>
+                                <div class="row" v-else>
+                                    <div class="col-md-6">
+                                        <CodeLoader/>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <CodeLoader/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        <div class="card shadow bg-success text-white w-100 mt-5">
+                            <div class="card-body">
+                                <div class="row" v-if="covidPerProv">
+                                    <div class="col-4">
+                                        <i class="fas fa-5x fa-heartbeat"></i>
+                                    </div>
+                                    <div class="col-8">
+                                        <h3>Recovered</h3>
+                                        <h3><b>{{new Intl.NumberFormat().format(covidPerProv[0].kasusSemb)}}</b></h3>
+                                    </div>
+                                </div>
+                                <div class="row" v-else>
+                                    <div class="col-md-6">
+                                        <CodeLoader/>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <CodeLoader/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        <div class="card shadow bg-warning text-white w-100 mt-5">
+                            <div class="card-body">
+                                <div class="row" v-if="covidPerProv">
+                                    <div class="col-4">
+                                        <i class="fas fa-5x fa-ambulance"></i>
+                                    </div>
+                                    <div class="col-8">
+                                        <h3>Deaths</h3>
+                                        <h3><b>{{new Intl.NumberFormat().format(covidPerProv[0].kasusMeni)}}</b></h3>
+                                    </div>
+                                </div>
+                                <div class="row" v-else>
+                                    <div class="col-md-6">
+                                        <CodeLoader/>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <CodeLoader/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <p class="ml-1 mt-4">Data Source : <br> https://indonesia-covid-19.mathdro.id/api/</p>
                     </div>
                 </div>
             </div>
@@ -55,7 +120,7 @@
 <script>
     //import content loader
     import {
-        ContentLoader ,
+        CodeLoader ,
     } from 'vue-content-loader'
 
     //import axios
@@ -69,7 +134,7 @@
         name: 'indonesia',
         components: {
             //loader component
-            ContentLoader ,
+            CodeLoader ,
             //component app
             Header,
             Footer
@@ -77,15 +142,60 @@
         data() {
             return {
                 //define properties
+                covidPerProv: null,
+                covdata : null,
                 covid19: null,
+                provs: null,
+                selectedProv:'Indonesia',
+            }
+        },
+        methods:{
+            getDataPerProv(){
+                if(this.selectedProv == 'Indonesia'){
+                    axios.get(`https://indonesia-covid-19.mathdro.id/api`).then(response => {     
+                        let dataAll = response.data;
+
+                        let dataIndo = [{
+                            "kasusPosi":dataAll.jumlahKasus,
+                            "kasusSemb":dataAll.sembuh,
+                            "kasusMeni":dataAll.meninggal,
+                        }];
+
+                        this.covidPerProv = dataIndo;
+                    })
+
+                    
+
+                }else{
+                    axios.get(`https://indonesia-covid-19.mathdro.id/api/provinsi/`).then(response => {     
+                        this.covidPerProv = response.data.data.filter(p => p.provinsi == this.selectedProv);
+                        console.log(this.covidPerProv);
+                    })
+                }
+                
+                
             }
         },
         created() {
-            //get post homepage
+            //get indo data
             axios.get('https://indonesia-covid-19.mathdro.id/api').then(response => {
                 this.covid19 = response.data;
                 console.log(this.covid19);
             })
-        }
+
+            //get provs
+            axios.get('https://indonesia-covid-19.mathdro.id/api/provinsi').then(response => {
+                this.provs = response.data.data;
+                console.log(this.provs);
+            })
+        },
+        watch: {
+            $route: {
+                immediate: true,
+                handler() {
+                    this.getDataPerProv()
+                }
+            }
+        }        
     }
 </script>
